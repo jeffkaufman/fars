74becc39c54552e70f6774f559c8b2edd1781dfa
Jeff Kaufman <jeff@jefftk.com>
2023-02-23 10:56:16 -0500

    revive this

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 for year in $(seq 1975 2020) ; do
     if [ ! -d $year ]; then
-        #wget https://static.nhtsa.gov/nhtsa/downloads/FARS/${year}/National/FARS${year}NationalCSV.zip
+        wget https://static.nhtsa.gov/nhtsa/downloads/FARS/${year}/National/FARS${year}NationalCSV.zip
         unzip -d $year FARS${year}NationalCSV.zip
         ./remove_nulls.py $year
     fi
 done
 
-./process.py
+./halloween-process.py
